e3a641566845d129ee33d3f8e1d327e7446c08b1
Pedrinhonitz <pedrohenriquekleinphg@gmail.com>
2024-11-09 18:49:42 -0300

    :sparkles: feat: Adding Functions Lambda, Equals and Sub from language

--- Interpreter/Interpreter.hs
+++ Interpreter/Interpreter.hs
@@ -1,25 +1,49 @@
-module Interpreter where
+module Interpreter where 
 
-import Lexer
-import TypeChecker
+import Lexer 
 
 isValue :: Expr -> Bool
-isValue BTrue = True
-isValue BFalse = True
-isValue (Num _) = True
-isValue _ = False
+isValue BTrue = True 
+isValue BFalse = True 
+isValue (Num _) = True 
+isValue _ = False 
+    
+subst :: String -> Expr -> Expr -> Expr
+subst x n b@(Var v) = if v == x then 
+                        n 
+                      else 
+                        b 
+subst x n (Add e1 e2) = (Add (subst x n e1) (subst x n e2))
+subst x n (And e1 e2) = (And (subst x n e1) (subst x n e2))
+subst x n (Eq e1 e2) = (Eq (subst x n e1) (subst x n e2))
+subst x n (If e e1 e2) = (If (subst x n e) (subst x n e1) (subst x n e2))
+subst x n (Lam v b) = (Lam v (subst x n b))
+subst x n (App e1 e2) = (App (subst x n e1) (subst x n e2))
+subst _ _ e = e
 
-step :: Expr -> Expr
+step :: Expr -> Expr 
 step (Add (Num n1) (Num n2)) = Num (n1 + n2)
-step (Add (Num n) e) = (Add (Num n) (step e))
-step (Add e1 e2) = Add (step e1) e2
-step (And BFalse e) = BFalse
-step (And BTrue e) = e
-step (And e1 e2) = And (step e1) e2
-step (If BTrue e1 e2) = e1
-step (If BFalse e1 e2) = e2
-step (If e e1 e2) = If (step e) e1 e2
+step (Add (Num n) e) = (Add (Num n) (step e)) 
+step (Add e1 e2) = Add (step e1) e2 
+step (Sub (Num n1) (Num n2)) = Num (n1 - n2)
+step (Sub (Num n) e) = (Sub (Num n) (step e)) 
+step (Sub e1 e2) = Sub (step e1) e2 
+step (And BFalse e) = BFalse 
+step (And BTrue e) = e 
+step (And e1 e2) = And (step e1) e2 
+step (Eq e1 e2) | isValue e1 && isValue e2 = if (e1 == e2) then 
+                                               BTrue 
+                                             else 
+                                               BFalse
+                | isValue e1 = Eq e1 (step e2)
+                | otherwise = Eq (step e1) e2
+step (If BTrue e1 e2) = e1 
+step (If BFalse e1 e2) = e2 
+step (If e e1 e2) = If (step e) e1 e2 
+step (App (Lam v b) e) | isValue e = subst v e b 
+                       | otherwise = (App (Lam v b) (step e))
+step (App e1 e2) = App (step e1) e2 
 
-eval :: Expr -> Expr
-eval exp = if isValue (exp) then exp
-                else eval (step exp)
+eval :: Expr -> Expr 
+eval e | isValue e = e 
+       | otherwise = eval (step e)
--- Interpreter/Lexer.hs
+++ Interpreter/Lexer.hs
@@ -1,13 +1,61 @@
-module Lexer where
+module Lexer where 
 
-data Expr = BTrue
-        | BFalse
-        | Num Int
-        | Add Expr Expr
-        | And Expr Expr
-        | If Expr Expr Expr
-        deriving Show
+import Data.Char
 
-data Ty = TBool
-        | TNum
+data Expr = BTrue 
+          | BFalse 
+          | Num Int 
+          | Add Expr Expr 
+          | Sub Expr Expr
+          | And Expr Expr 
+          | Eq Expr Expr
+          | If Expr Expr Expr 
+          | Var String 
+          | Lam String Expr 
+          | App Expr Expr
+          deriving (Show, Eq)
+
+data Ty = TBool 
+        | TNum 
         deriving (Show, Eq)
+
+data Token = TokenTrue
+           | TokenFalse 
+           | TokenNum Int 
+           | TokenAdd 
+           | TokenSub
+           | TokenAnd 
+           | TokenEq
+           | TokenIf
+           | TokenThen
+           | TokenElse 
+           | TokenVar String
+           | TokenLam 
+           | TokenArrow 
+           deriving Show
+
+lexer :: String -> [Token]
+lexer [] = [] 
+lexer ('+':cs) = TokenAdd : lexer cs 
+lexer ('\\':cs) = TokenLam : lexer cs 
+lexer ('=':'=':cs) = TokenEq : lexer cs 
+lexer ('-':'>':cs) = TokenArrow : lexer cs 
+lexer ('-':cs) = TokenSub : lexer cs 
+lexer (c:cs) 
+   | isSpace c = lexer cs 
+   | isAlpha c = lexerKW (c:cs) 
+   | isDigit c = lexerNum (c:cs)
+
+lexerNum :: String -> [Token]
+lexerNum cs = case span isDigit cs of 
+                (num, rest) -> TokenNum (read num) : lexer rest
+
+lexerKW :: String -> [Token]
+lexerKW cs = case span isAlpha cs of 
+               ("true", rest) -> TokenTrue : lexer rest 
+               ("false", rest) -> TokenFalse : lexer rest 
+               ("and", rest) -> TokenAnd : lexer rest 
+               ("if", rest) -> TokenIf : lexer rest 
+               ("then", rest) -> TokenThen : lexer rest 
+               ("else", rest) -> TokenElse : lexer rest 
+               (var, rest) -> TokenVar var : lexer rest
